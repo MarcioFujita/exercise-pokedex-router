68a12361625e34897760014ea765cbdd5c85d2a3
MarcioFujita <hanker78@gmail.com>
2021-03-02 18:49:23 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <BrowserRouter>
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} />
+      </BrowserRouter>
     </div>
   );
 }
